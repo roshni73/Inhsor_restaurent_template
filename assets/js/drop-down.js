fca00ba93508a65dc90864bfd099c845c64f9c59
roshni poudel <99161691+roshni73@users.noreply.github.com>
2023-11-17 11:04:14 +0545

    first commit

---
+++ assets/js/drop-down.js
@@ -0,0 +1,8 @@
+function showDropDown() {
+    var x = document.getElementById("drop_down_links");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
